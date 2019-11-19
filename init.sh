c791da762ff46b9bda05f8ad5b1f57ba745dab36
Jorge Aznar <jorgeatgu@gmail.com>
2019-11-19 23:15:49 +0100

    bash and gif

--- init.sh
+++ init.sh
@@ -2,12 +2,13 @@
 
 GREEN='\033[00;32m'
 LRED='\033[01;31m'
+RED='\033[00;31m'
 PURPLE='\033[00;35m'
 CYAN='\033[00;36m'
+YELLOW='\033[00;33m'
 
 function initRollup {
-    echo -n "
-    ${CYAN} Vamos a lanzar base Rollup d3 ¿ya has creado una carpeta? (s/n): "
+    echo -n "${CYAN}Vamos a lanzar base Rollup d3 ¿ya has creado una carpeta? (s/n): "
 
     function launch {
       mkdir css src js img &&
@@ -18,28 +19,24 @@ function initRollup {
       curl -O "https://raw.githubusercontent.com/jorgeatgu/base-rollup-d3/master/{_variables.css,styles.css}" &&
       cd ../js &&
       curl -O "https://raw.githubusercontent.com/jorgeatgu/base-rollup-d3/master/src/{d3.js,index.js}" &&
-      cd .. &&
-      cd .. &&
+      cd ../.. &&
       git init &&
       git add . &&
       git commit -m 'init' &&
-      echo -e "
-      ${LRED} Esto va a costar un pocor 🚨🚨" &&
+      echo -e "${LRED}Esto va a costar un pocor 🚨🚨${RED}" &&
       yarn install &&
-      echo -e "
-      ${GREEN} El script ha terminado. Es hora de picar código! \U0001f913\n" &&
-      afplay /System/Library/Sounds/Submarine.aiff
+      echo -e "${GREEN}El script ha terminado. Es hora de picar código! \U0001f913\n" &&
+      afplay /System/Library/Sounds/Submarine.aiff &&
+      yarn serve
     }
 
     read -r answer
         if echo "$answer" | grep -iq "^s" ;
     then
-        echo -e "
-        ${GREEN} Perfecto, vamos a comenzar con Base Rollup d3"
+        echo -e "${GREEN} Perfecto, vamos a comenzar con Base Rollup d3"
         launch;
     else
-        echo -n "
-        ${PURPLE}Pues elige el nombre de tu proyecto: "
+        echo -n "${PURPLE}Pues elige el nombre de tu proyecto: ${YELLOW}"
         read -r var_name &&
         mkdir "$var_name" &&
         cd "$var_name" &&
